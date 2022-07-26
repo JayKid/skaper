e3671c0182c834ad5a341dc153d4ab17eb59dcf7
Josep López <cyph3rfox@gmail.com>
2022-07-26 13:36:16 +0200

    add some more structure

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,95 @@
-// use std::io::{stdin, stdout};
+extern crate termion;
+
 use colored::*;
+use std::cmp::{max, min};
+use std::io::{stdin, stdout, Stdout, Write};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::{IntoRawMode, RawTerminal};
+
+struct Choice {
+    name: String,
+    action: Box<dyn Fn() -> ()>,
+}
 
 struct Ui {
-    // current_item_selected: usize,
     caret: ColoredString,
+    options: Vec<Choice>,
+    current_index_selected: usize,
+    stdout: RawTerminal<Stdout>,
 }
 
 impl Ui {
-    fn print_item(&self, item: &str) {
-        println!("{}", item);
+    fn init(&mut self, options: Vec<Choice>, stdout: RawTerminal<Stdout>) {
+        self.options = options;
+        self.stdout = stdout;
     }
 
-    fn print_selected_item(&self, item: &str) {
-        println!("{}{}", self.caret, String::from(item).cyan());
-    }
-
-    fn render_options(&self, options: Vec<&str>) {
+    fn refresh(&mut self) {
         let mut index: usize = 0;
-        while index < options.len() {
-            if index == 1 {
-                self.print_selected_item(options[index])
+        self.move_caret_back();
+        while index < self.options.len() {
+            if index == self.current_index_selected {
+                self.print_selected_item(String::from(&self.options[index].name));
             } else {
-                self.print_item(options[index])
+                self.print_item(String::from(&self.options[index].name));
             }
             index = index + 1;
+        }
+    }
+
+    fn navigate_up(&mut self) {
+        if self.current_index_selected != 0 {
+            self.current_index_selected = max(0, self.current_index_selected - 1);
+        }
+        self.refresh();
+    }
+
+    fn navigate_down(&mut self) {
+        self.current_index_selected = min(self.current_index_selected + 1, self.options.len() - 1);
+        self.refresh();
+    }
+
+    fn print_item(&mut self, item: String) {
+        write!(self.stdout, "{}\n", item).unwrap(); // New way of doing things in Termion
+    }
+
+    fn print_selected_item(&self, item: String) {
+        println!("{} {}", String::from(item).cyan(), self.caret); // Old (?) way of doing things in Termion
+    }
+
+    fn move_caret_back(&mut self) {
+        write!(
+            self.stdout,
+            "{}{}",
+            termion::cursor::Goto(1, 1),
+            termion::clear::All
+        )
+        .unwrap();
+    }
+
+    fn clear_current_line(&mut self) {
+        write!(self.stdout, "{}", termion::clear::CurrentLine).unwrap();
+    }
+
+    fn select_option(&mut self) {
+        let results = (self.options[self.current_index_selected].action)();
+        write!(self.stdout, "{:?}", results).unwrap();
+    }
+
+    fn listen_for_keys(&mut self) {
+        let stdin = stdin();
+
+        for c in stdin.keys() {
+            self.clear_current_line();
+            match c.unwrap() {
+                Key::Char('q') => break,
+                Key::Esc => break,
+                Key::Up => self.navigate_up(),
+                Key::Down => self.navigate_down(),
+                Key::Right => self.select_option(),
+                _ => {}
+            }
         }
     }
 }
@@ -31,26 +97,39 @@ impl Ui {
 impl Default for Ui {
     fn default() -> Ui {
         Ui {
-            caret: String::from("> ").magenta(),
+            caret: String::from("<-").magenta(),
+            options: Vec::from([]),
+            current_index_selected: 0,
+            stdout: stdout().into_raw_mode().unwrap(),
         }
     }
 }
 
-fn main() {
-    // let stdin = stdin();
-    // let mut stdout = stdout();
-    // let current_item_selected: usize = 0;
+pub fn get_results() {
+    vec!["a", "b", "c"];
+}
 
-    // for character in stdin.keys() {
-    //     match character.unwrap() {
-    //         Key::Char('q') => break,
-    //         _ => {}
-    //     }
-    // }
-    let ui_service = Ui {
+fn main() {
+    let stdout = stdout().into_raw_mode().unwrap();
+
+    let mut ui_service = Ui {
         ..Default::default()
     };
-    let options = vec!["1st option", "2nd option", "3rd option"];
-
-    ui_service.render_options(options);
+    let options = vec![
+        Choice {
+            name: String::from("3090 TI"),
+            action: Box::new(get_results),
+        },
+        Choice {
+            name: String::from("3090"),
+            action: Box::new(get_results),
+        },
+        Choice {
+            name: String::from("3080 TI"),
+            action: Box::new(get_results),
+        },
+    ];
+    ui_service.init(options, stdout);
+    ui_service.refresh();
+    ui_service.listen_for_keys();
 }
