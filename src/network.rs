baf2935eb391ad6a573e03652b43a9b5e4116862
Josep López <cyph3rfox@gmail.com>
2022-07-27 12:14:55 +0200

    add loading debug indicator within network module

--- src/network.rs
+++ src/network.rs
@@ -1,4 +1,6 @@
 pub fn fetch(url: String) -> String {
+    println!("Loading URL...\r");
     let response = reqwest::blocking::get(url).unwrap().text().unwrap();
+    println!("URL loaded!\r");
     return response;
 }
