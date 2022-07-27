8f007e47d0f460b1f16b5a17279e02e3e8e76320
Josep López <cyph3rfox@gmail.com>
2022-07-27 12:00:28 +0200

    extract UI to own module and fetch URL contents via internet (part II)

--- src/main.rs
+++ src/main.rs
@@ -1,124 +1,12 @@
 extern crate termion;
 
-// bringing the module into scope
 mod html_helper;
-use html_helper::htmlutils::get_example_html;
+mod network;
+mod ui;
+use ui::{Ui, Choice};
 
-// Terminal I/O crates
-use colored::*;
-use std::cmp::{max, min};
-use std::io::{stdin, stdout, Stdout, Write};
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::{IntoRawMode, RawTerminal};
-
-use crate::html_helper::htmlhelper::get_html_handler;
-
-struct Choice {
-    name: String,
-    action: Box<dyn Fn() -> ()>,
-}
-
-struct Ui {
-    caret: ColoredString,
-    options: Vec<Choice>,
-    current_index_selected: usize,
-    stdout: RawTerminal<Stdout>,
-}
-
-impl Ui {
-    fn init(&mut self, options: Vec<Choice>, stdout: RawTerminal<Stdout>) {
-        self.options = options;
-        self.stdout = stdout;
-    }
-
-    fn refresh(&mut self) {
-        let mut index: usize = 0;
-        self.move_caret_back();
-        while index < self.options.len() {
-            if index == self.current_index_selected {
-                self.print_selected_item(String::from(&self.options[index].name));
-            } else {
-                self.print_item(String::from(&self.options[index].name));
-            }
-            index = index + 1;
-        }
-    }
-
-    fn navigate_up(&mut self) {
-        if self.current_index_selected != 0 {
-            self.current_index_selected = max(0, self.current_index_selected - 1);
-        }
-        self.refresh();
-    }
-
-    fn navigate_down(&mut self) {
-        self.current_index_selected = min(self.current_index_selected + 1, self.options.len() - 1);
-        self.refresh();
-    }
-
-    fn print_item(&mut self, item: String) {
-        write!(self.stdout, "{}\r\n", item).unwrap();
-        self.stdout.flush().unwrap();
-    }
-
-    fn print_selected_item(&mut self, item: String) {
-        write!(
-            self.stdout,
-            "{}{}\r\n",
-            String::from(item).cyan(),
-            self.caret
-        )
-        .unwrap();
-        self.stdout.flush().unwrap();
-    }
-
-    fn move_caret_back(&mut self) {
-        write!(
-            self.stdout,
-            "{}{}",
-            termion::cursor::Goto(1, 1),
-            termion::clear::All
-        )
-        .unwrap();
-    }
-
-    fn clear_current_line(&mut self) {
-        write!(self.stdout, "{}", termion::clear::CurrentLine).unwrap();
-    }
-
-    fn select_option(&mut self) {
-        let results = (self.options[self.current_index_selected].action)();
-        write!(self.stdout, "{:?}", results).unwrap();
-    }
-
-    fn listen_for_keys(&mut self) {
-        let stdin = stdin();
-
-        for c in stdin.keys() {
-            self.clear_current_line();
-            match c.unwrap() {
-                Key::Char('q') => break,
-                Key::Esc => break,
-                Key::Up => self.navigate_up(),
-                Key::Down => self.navigate_down(),
-                Key::Right => self.select_option(),
-                _ => {}
-            }
-        }
-    }
-}
-
-impl Default for Ui {
-    fn default() -> Ui {
-        Ui {
-            caret: String::from(" <-").magenta(),
-            options: Vec::from([]),
-            current_index_selected: 0,
-            stdout: stdout().into_raw_mode().unwrap(),
-        }
-    }
-}
+use std::io::{stdout};
+use termion::raw::{IntoRawMode};
 
 pub fn get_results() {
     vec!["a", "b", "c"];
@@ -133,27 +21,28 @@ fn main() {
     let options = vec![
         Choice {
             name: String::from("3090 TI"),
-            action: Box::new(something),
+            action: Box::new(action_3090_ti),
         },
         Choice {
             name: String::from("3090"),
-            action: Box::new(something),
+            action: Box::new(action_3090_ti),
         },
         Choice {
             name: String::from("3080 TI"),
-            action: Box::new(something),
+            action: Box::new(action_3090_ti),
         },
     ];
     ui_service.init(options, stdout);
     ui_service.refresh();
-    ui_service.listen_for_keys();
 }
 
-fn something() {
-    // TO-DO: Move this to network calls
-    let website_contents = get_example_html();
+fn action_3090_ti() {
+    let website_contents = network::fetch(String::from("https://www.orderflow.ch/de/search?limit=100&sort=price%7Casc&q=3090+ti&cat=%5BPC-Grafikkarten%5D"));
 
-    let mut html_helper = get_html_handler();
+    let mut html_helper = html_helper::htmlhelper::get_instance();
     html_helper.parse_html(website_contents);
+
+    // Ideally we would return results here that we can process/display however we want
     let selector = html_helper.select(".item.media .first_price>.price".to_string());
+    // Display that currently happens within select should happen below
 }
